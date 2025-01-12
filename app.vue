28ef9e9d20ce907a9701b6b2d239118e80e427df
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-01-12 10:04:14 +0300

    File-based routing. Dynamic routing.

--- app.vue
+++ app.vue
@@ -3,7 +3,6 @@
     <nav>
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/movies">Movies</NuxtLink>
-      <NuxtLink to="/teams">Team data</NuxtLink>
       <NuxtLink to="https://google.com">google</NuxtLink>
     </nav>
     <NuxtPage />
