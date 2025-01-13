e6eeeb64921f460e3234d8e61267fb1c873f2ecf
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-01-13 08:05:04 +0300

    получение данных через useFetch

--- pages/movies/[movieId].vue
+++ pages/movies/[movieId].vue
@@ -1,14 +1,36 @@
-<script setup lang="ts">
-
-const route = useRoute()
-
-const {data} = useAsyncData(`/movies/${route.params.movieId}`, () => {
-  return $fetch(`http://www.omdbapi.com/?apikey=876937fd&i=${route.params.movieId}`)
-})
+<script setup>
+const route = useRoute();
+const { data } = useFetch(
+  `http://www.omdbapi.com/?apikey=876937fd&i=${route.params.movieId}`,
+  {
+    pick: ["Title"],
+    key: `/movies/${route.params.movieId}`,
+  },
+);
+/*
+const { data } = useAsyncData(
+  `/movies/${route.params.movieId}`,
+  () => {
+    return $fetch(
+      `http://www.omdbapi.com/?apikey=876937fd&i=${route.params.movieId}`,
+    );
+  },
+  {
+    server: false,
+    pick: ["Title"],
+    // transform(data) {
+    //   return {
+    //     Plot: data.Plot,
+    //     Title: data.Title,
+    //   };
+    // },
+  },
+);
+*/
 </script>
 
 <template>
   <pre>
-    {{data?.Title}}
+    {{ data }}
   </pre>
 </template>
