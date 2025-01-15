4bbc748155ecf6ca2e393b5334496a609920fc2d
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-01-15 08:10:56 +0300

    обработка ответа в useFetch

--- pages/movies/[movieId].vue
+++ pages/movies/[movieId].vue
@@ -5,6 +5,13 @@ const { data } = useFetch(
   {
     pick: ["Title"],
     key: `/movies/${route.params.movieId}`,
+    onResponse({ request, response }) {
+      console.log(response._data);
+      if (response._data.Error === "Incorrect IMDb ID.") {
+        showError({ statusCode: 404, statusMessage: "Page not found1" });
+      }
+    },
+    server: true,
   },
 );
 /*
