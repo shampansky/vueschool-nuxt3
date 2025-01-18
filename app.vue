cc857251711ed290b36e567fedbe5020099b307a
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-01-18 14:59:28 +0300

    custom page layouts

--- app.vue
+++ app.vue
@@ -2,17 +2,14 @@
 useHead({
   // title: 'My movie site',
   titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} - My movie site` : 'My movie site'
-  }
-})
+    return titleChunk ? `${titleChunk} - My movie site` : "My movie site";
+  },
+});
 </script>
 <template>
   <div>
-    <nav>
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/movies">Movies</NuxtLink>
-      <NuxtLink to="https://google.com">google</NuxtLink>
-    </nav>
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/movies">Movies</NuxtLink>
+    <NuxtLink to="https://google.com">google</NuxtLink>
+  </nav>
+  test
+  <slot></slot>
+</template>
