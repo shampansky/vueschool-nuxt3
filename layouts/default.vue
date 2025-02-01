d61d068a4e89bb6fc8c6b82e0f00dd1f0cf7ba57
Evgeniy Shadrin <e.shadrin@aristos.pw>
2025-02-01 10:18:03 +0300

    управление состоянием через useState

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+const isLoggedIn = useIsLoggedIn();
+</script>
+
 <template>
   <nav>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/movies">Movies</NuxtLink>
     <NuxtLink to="https://google.com">google</NuxtLink>
+    <NuxtLink v-if="!isLoggedIn" to="/login">Login</NuxtLink>
+    <a @click.prevent="isLoggedIn = false" href="#" v-else>Logout</a>
   </nav>
   test
   <slot></slot>
